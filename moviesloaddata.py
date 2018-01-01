28a6d7d727786857a9cdd6c97a18e69396240974
Tope.O <oyenuga.temitope@yahoo.com>
2018-01-01 15:51:02 +0100

    Update moviesloaddata.py

--- moviesloaddata.py
+++ moviesloaddata.py
@@ -3,7 +3,7 @@ import boto3
 import json
 import decimal
 
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 
 table = dynamodb.Table('Movies')
 
